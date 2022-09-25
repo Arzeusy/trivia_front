74211704aeab3f4f9f8c17706cb583131e070581
arzeusy <jkob1994@gmail.com>
2022-09-24 20:09:14 -0600

    actualizacion the url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://triviabackrickmorty.herokuapp.com/api',
   apiUrlRM: 'https://rickandmortyapi.com/api/episode'
 
 };
